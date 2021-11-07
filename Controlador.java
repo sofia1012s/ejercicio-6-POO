cd2cc89f5f1f615b1ffd0eadd4c3eeda60686c2e
Sofía Salguero <salguerosofia.1012@gmail.com>
2021-11-06 20:59:44 -0600

    Update Controlador.java

--- Controlador.java
+++ Controlador.java
@@ -1,3 +1,42 @@
+import java.util.*;
+
 public class Controlador {
-    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int opcionMenu = 0;
+        boolean p = false;
+
+        Vista vis = new Vista();
+        vis.Inicio();
+
+        while (opcionMenu != 3) {
+            do {
+                try {
+                    vis = new Vista();
+                    opcionMenu = vis.Menu(opcionMenu);
+                    p = true;
+                } catch (InputMismatchException ex) {
+                    System.out.println("\nPor favor, ingresa un numero\n");
+                    p = false;
+                }
+            } while (!p);
+
+            switch (opcionMenu) {
+            case 1: // Realizar una compra
+            vis.productos();
+                break;
+
+            case 2: // Ver carrito de compras
+                break;
+
+            case 3: // Salir
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
 }
